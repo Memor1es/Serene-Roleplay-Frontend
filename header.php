a78202de56ce6f820e0ca94aafed8b2304a9dd1b
Billy Meis <54307189+TheBillyIsMe@users.noreply.github.com>
2020-01-26 18:56:32 -0500

    Header + Hero + Features

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo $pageName?> | Serene Roleplay</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="title" content="Serene Roleplay | Roleplay Reimagined">
+<meta name="description" content="The Serene Roleplay Website.">
+<meta name="keywords" content=" FiveM, GTA RP, GTA Roleplay, GTA V, Roleplay, Serene, Serene Roleplay, SereneRP">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="English">
+<meta name="author" content="BillyIsMe">
+  <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+<link rel="stylesheet" href="assets/css/uikit.min.css" />
+<script src="assets/js/uikit.min.js"></script>
+<script src="assets/js/uikit-icons.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
+  </head>
+  <body>
+    <nav class="uk-navbar-container uk-margin uk-navbar-transparent uk-margin-remove-bottom uk-light" uk-navbar>
+        <div class="uk-navbar-left uk-margin-large-left uk-margin-small-top">
+            <a class="uk-navbar-item uk-logo" href="/">            <img data-src="assets/img/banner.png" width="65%" height="65%" alt="Serene Roleplay | Roleplay Re-imagined" uk-img></a>
+        </div>
+        <div class="uk-navbar-right uk-margin-large-right">
+          <div class="uk-navbar-item">
+              <div class="uk-text-small uk-text-bold <?php if (strtolower($pageName) == 'home'){echo ' uk-text-primary';}?>"><a class="uk-text-emphasis" href="#">Home</a></div>
+          </div>
+            <div class="uk-navbar-item">
+                <div class="uk-text-small uk-text-bold"><a class="uk-text-emphasis" href="#">Rules</a></div>
+            </div>
+            <div class="uk-navbar-item">
+                <div class="uk-text-small uk-text-bold"><a class="uk-text-emphasis" href="#">Docs</a></div>
+            </div>
+        </div>
+    </nav>
